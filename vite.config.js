c46ea594625cf07b6c92e29b7565d47b7c75c631
jpselas05 <jp.sm0509@gmail.com>
2026-01-03 19:08:28 -0300

    agoravai

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: { outDir: 'docs' },
   base: '/ramlive-erp/', // substitua pelo nome do seu repositório no GitHub
   server: {
     port: 5173,
